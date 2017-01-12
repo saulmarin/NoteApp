7f5bdba22d0702bfe1ac95a977885a9a187b1594
Saul Marin <sau7.mar1n@gmail.com>
2017-01-12 14:15:09 +0100

    Added code to save in defaults

--- app/src/main/java/com/marin/saul/noteapp/NoteListFragment.java
+++ app/src/main/java/com/marin/saul/noteapp/NoteListFragment.java
@@ -7,10 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class NoteListFragment extends Fragment {
 
 
@@ -23,7 +19,9 @@ public class NoteListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_note_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_note_list, container, false);
+
+        return view;
     }
 
 }
